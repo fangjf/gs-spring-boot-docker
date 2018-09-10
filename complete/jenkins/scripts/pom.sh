e3d54779eb69d7306c3945cee94b039538c6d240
方俊锋 <fangjunfeng@qianmi.com>
2018-09-10 17:48:34 +0800

    输出 pom 的 project.artifactId project.version 为环境变量

---
+++ complete/jenkins/scripts/pom.sh
@@ -0,0 +1,7 @@
+set -x
+ARTIFACTID=`mvn help:evaluate -Dexpression=project.artifactId | grep "^[^\[]"`
+set +x
+
+set -x
+VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+set +x
